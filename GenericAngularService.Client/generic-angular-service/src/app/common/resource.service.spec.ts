5569f4e223094edb6da4c8fd8b885d83ac230178
Mateusz Kikmunter <mateusz.kikmunter@willistowerswatson.com>
2018-09-18 12:07:43 +0100

    added test for getall

--- GenericAngularService.Client/generic-angular-service/src/app/common/resource.service.spec.ts
+++ GenericAngularService.Client/generic-angular-service/src/app/common/resource.service.spec.ts
@@ -33,4 +33,12 @@ describe("ResourceService", () => {
     req.flush({ id: 1, firstName: "Luke", lastName: "Skywalker", company: "Star Wars" });
   }));
 
+  it("should get all records with http.get", inject([HttpTestingController, FakeService], (httpMock: HttpTestingController, service: FakeService) => {
+    service.getAll().subscribe(data => {
+      expect(data.length).toBe(2);
+    });
+
+    const req = httpMock.expectOne("/api/fake");
+    req.flush([{ id: 1 }, { id: 2 }]);
+  }));
 });
