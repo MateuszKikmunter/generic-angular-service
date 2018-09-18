4c4c7e9502afc7bbe8304a8a3d94538a07742502
Mateusz Kikmunter <mateusz.kikmunter@willistowerswatson.com>
2018-09-18 16:50:58 +0100

    added missing expectations in error handling interceptor

--- GenericAngularService.Client/generic-angular-service/src/app/common/handle-http-error.interceptor.spec.ts
+++ GenericAngularService.Client/generic-angular-service/src/app/common/handle-http-error.interceptor.spec.ts
@@ -19,6 +19,8 @@ describe("handle-http-error-interceptor", () => {
                 }
             ]
         });
+
+        spyOn(console, "log");
     });
 
     afterEach(inject([HttpTestingController], (mockHttp: HttpTestingController) => {
@@ -26,10 +28,16 @@ describe("handle-http-error-interceptor", () => {
     }));
 
     it("should handle error", inject([HttpTestingController, HttpClient], (mockHttp: HttpTestingController, mockHttpClient: HttpClient) => {
-        mockHttpClient.get("/error").subscribe();
+        mockHttpClient.get("/error").subscribe((next) => {
+            expect(next["status"]).toBe(404);
+            expect(next["statusText"]).toBe("Not Found");
+        });
+
         mockHttp.expectOne("/error").error(new ErrorEvent("Not Found"), {
             status: 404,
             statusText: "Not Found"
         });
+
+        expect(console.log).toHaveBeenCalled();
     }));
 });
