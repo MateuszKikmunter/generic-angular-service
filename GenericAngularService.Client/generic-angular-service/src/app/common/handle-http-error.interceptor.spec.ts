ca107774d9fb86cf129ba7d53d2cbc3f36bdadf3
Mateusz Kikmunter <mateusz.kikmunter@willistowerswatson.com>
2018-10-11 15:40:40 +0100

    added tests for http 422

--- GenericAngularService.Client/generic-angular-service/src/app/common/handle-http-error.interceptor.spec.ts
+++ GenericAngularService.Client/generic-angular-service/src/app/common/handle-http-error.interceptor.spec.ts
@@ -30,6 +30,7 @@ describe("handle-http-error-interceptor", () => {
     it("should handle error", async(inject([HttpTestingController, HttpClient], (mockHttp: HttpTestingController, mockHttpClient: HttpClient) => {
         mockHttpClient.get("/error").subscribe(() => { }, errorResponse => {
             expect(errorResponse).toBeTruthy();
+            expect(errorResponse).toContain('Something went wrong, please try again.');
         });
 
         mockHttp.expectOne("/error").error(new ErrorEvent("Not Found"), {
@@ -39,5 +40,17 @@ describe("handle-http-error-interceptor", () => {
 
         expect(console.log).toHaveBeenCalledTimes(1);
         expect(console.log).toHaveBeenCalledWith("Http error (client/network). Http failure response for /error: 404 Not Found");
+    })));
+
+    it("it should handle status 422", async(inject([HttpTestingController, HttpClient], (http: HttpTestingController, httpClient: HttpClient) => {
+        httpClient.post("/invalid-model", {}, { observe: 'response' }).subscribe(() => { }, err => {
+            expect(err).toBeDefined();
+            expect(err).toBeTruthy();
+        });
+
+        http.expectOne("/invalid-model").error(new ErrorEvent("Unprocessable Entity"), {
+            status: 422,
+            statusText: "Unprocessable Entity"
+        });
     })));
 });
