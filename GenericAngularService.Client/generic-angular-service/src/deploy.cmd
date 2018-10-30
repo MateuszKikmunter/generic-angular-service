534acc7193b10c59415776062cc8043dd9bf2cf3
m.kikmunter <M.Kikmunter@gmail.com>
2018-10-30 20:24:55 +0000

    forgot to swap angular-cli.json to angular.json in kudu script

--- GenericAngularService.Client/generic-angular-service/src/deploy.cmd
+++ GenericAngularService.Client/generic-angular-service/src/deploy.cmd
@@ -100,7 +100,7 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
 )
 
 :: 3. Angular Prod Build
-IF EXIST "%DEPLOYMENT_SOURCE%/.angular-cli.json" (
+IF EXIST "%DEPLOYMENT_SOURCE%/.angular.json" (
 echo Building App in %DEPLOYMENT_SOURCE%…
 pushd "%DEPLOYMENT_SOURCE%"
 call :ExecuteCmd !NPM_CMD! run build
