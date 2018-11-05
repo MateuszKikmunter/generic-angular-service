665c153c00736543cd67e1450b2f66c87a5e99ee
Mateusz Kikmunter <mateusz.kikmunter@willistowerswatson.com>
2018-11-05 16:51:45 +0000

    added tests for cache

--- GenericAngularService.Client/generic-angular-service/src/app/common/cache/cache-interceptor.spec.ts
+++ GenericAngularService.Client/generic-angular-service/src/app/common/cache/cache-interceptor.spec.ts
@@ -1,29 +1,68 @@
-// import { TestBed, inject, async } from "@angular/core/testing";
-// import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
-// import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { TestBed, inject } from "@angular/core/testing";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { HTTP_INTERCEPTORS, HttpClient, HttpResponse, HttpRequest } from "@angular/common/http";
 
-// import { CacheInterceptor } from "./cache-interceptor";
-// import { HttpCacheService } from './http-cache.service';
+import { CacheInterceptor } from "./cache-interceptor";
+import { HttpCacheService } from './http-cache.service';
+import { CacheEntry } from "./cache.entry";
+import { DataTablesOptions } from "../angular-datatables/data-tables.options";
+import { url } from "inspector";
 
-// describe("cache-interceptor", () => {
+describe("cache-interceptor", () => {
+    let dataTableUrl = "api/GetTableData";
+    let fakeUrl = "api/fake";
+    let post = "POST";
+    
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule
+            ],
+            providers: [
+                {
+                    provide: HTTP_INTERCEPTORS,
+                    useClass: CacheInterceptor,
+                    multi: true
+                },
+                HttpCacheService
+            ]
+        });
+    });
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({
-//             imports: [
-//                 HttpClientTestingModule
-//             ],
-//             providers: [
-//                 {
-//                     provide: HTTP_INTERCEPTORS,
-//                     useClass: CacheInterceptor,
-//                     multi: true
-//                 },
-//                 HttpCacheService
-//             ]
-//         });
-//     });
+    afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
+        httpMock.verify();
+    }));
 
-//     afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
-//         httpMock.verify();
-//     }));
-// });
+    it("should return data from server when non cacheable request", inject([HttpTestingController, HttpClient], (mockHttpController: HttpTestingController, mockHttpClient: HttpClient) => {
+        mockHttpClient.post(fakeUrl, {}).subscribe(res => {
+            expect(res["id"]).toBe(1);
+            expect(res["name"]).toBe("test");
+        });
+
+        const req = mockHttpController.expectOne(fakeUrl);
+        expect(req.request.method).toBe(post);
+
+        req.flush({ id: 1, name: "test" })
+    }));
+
+    it("should return udefined when no correspondent item in cache", inject([HttpTestingController, HttpClient], (mockHttpController: HttpTestingController, mockHttpClient: HttpClient) => {
+        mockHttpClient.post(dataTableUrl, {}).subscribe(res => expect(res).toBeUndefined());
+
+        const req = mockHttpController.expectOne(dataTableUrl);
+        expect(req.request.method).toBe(post);
+    }));
+
+    it("should return cached item", inject([HttpTestingController, HttpClient], (mockHttpController: HttpTestingController, mockHttpClient: HttpClient) => {
+        let service = new HttpCacheService();
+        let dtOptions = new DataTablesOptions();
+        let request = new HttpRequest(post, dataTableUrl, { body: dtOptions });
+        let entry: CacheEntry = { url: dataTableUrl, options: dtOptions, reponse: new HttpResponse<any>() };
+
+        service.put(request, entry.reponse);
+        mockHttpClient.post(dataTableUrl, dtOptions, {}).subscribe(res => expect(res).toEqual(entry));
+
+        const req = mockHttpController.expectOne(dataTableUrl);
+        expect(req.request.method).toBe(post);
+        expect(req.request.body).toBe(dtOptions);
+    }));
+});
--- GenericAngularService.Client/generic-angular-service/src/app/common/cache/http-cache.service.spec.ts
+++ GenericAngularService.Client/generic-angular-service/src/app/common/cache/http-cache.service.spec.ts
@@ -1,12 +1,43 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HttpCacheService } from './http-cache.service';
+import { DataTablesOptions } from '../angular-datatables/data-tables.options';
+import { HttpResponse, HttpRequest } from '@angular/common/http';
 
 describe('HttpCacheService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: HttpCacheService;
+
+  beforeEach(() => 
+  TestBed.configureTestingModule({
+    providers: [
+      HttpCacheService
+    ]
+  }));
+
+  beforeEach(() => {
+    service = new HttpCacheService();
+  });
 
   it('should be created', () => {
     const service: HttpCacheService = TestBed.get(HttpCacheService);
     expect(service).toBeTruthy();
+  });
+
+  it("should add item to cache and then return it", () => {
+    let response = new HttpResponse<any>();
+    let request = new HttpRequest<any>("POST", "api/GetTableData", new DataTablesOptions(), {});
+    
+    service.put(request, new HttpResponse<any>());
+    let result = service.get(request);
+    expect(result).toBeTruthy();
+    expect(result).toEqual(response);
+  });
+
+  it("should reset cache", () => {
+    let request = new HttpRequest<any>("POST", "api/GetTableData", new DataTablesOptions(), {});
+    service.put(request, new HttpResponse<any>());
+
+    service.invalidateCache();
+    expect(service.get(request)).toBeUndefined();
   });
 });
