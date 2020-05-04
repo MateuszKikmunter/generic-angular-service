781a078429a791e4b829a984e2e361cf951d4145
m.kikmunter <M.Kikmunter@gmail.com>
2020-05-04 12:41:49 +0100

    seed companies script

---
+++ GenericAngularService.ExpressApi/src/data/seed.companies.sql
@@ -0,0 +1,17 @@
+INSERT INTO AngularGenericService.dbo.Companies
+VALUES ('Microsoft', 'IT', CAST('1975-04-04 00:00' as datetimeoffset));
+
+INSERT INTO AngularGenericService.dbo.Companies
+VALUES ('Apple', 'IT', CAST('1976-04-01 00:00:00' as datetimeoffset));
+
+INSERT INTO AngularGenericService.dbo.Companies
+VALUES ('Oracle', 'IT', CAST('1977-06-16 00:00:00' as datetimeoffset));
+
+INSERT INTO AngularGenericService.dbo.Companies
+VALUES ('IBM', 'IT', CAST('1911-06-16 00:00:00' as datetimeoffset));
+
+INSERT INTO AngularGenericService.dbo.Companies
+VALUES ('Amazon', 'IT', CAST('1994-07-05 00:00:00' as datetimeoffset));
+
+INSERT INTO AngularGenericService.dbo.Companies
+VALUES ('Facebook', 'IT', CAST('04/04/1975 00:00:00' as datetimeoffset));
