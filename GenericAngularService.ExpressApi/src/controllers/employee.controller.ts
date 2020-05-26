bc95b95c9aa66f9ac5fac06cd5f58d95c0fe6af9
m.kikmunter <M.Kikmunter@gmail.com>
2020-05-26 15:26:09 +0100

    added employee server side validation

--- GenericAngularService.ExpressApi/src/controllers/employee.controller.ts
+++ GenericAngularService.ExpressApi/src/controllers/employee.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response, Router, NextFunction } from 'express';
 
+import { validationResult, check, ValidationChain } from "express-validator";
+
 import { DataTablesOptions } from './../models/data-tables/data-tables.options';
 import { EmployeeRepository } from '../services/employee.repository';
 import { HttpCode } from './../utils/enums/http.code';
@@ -16,15 +18,27 @@ export class EmployeeController {
     }
 
     private initRoutes(): void {
-        this.router.post(`${this.baseUrl}/GetTableData`, async (req, res, next) => {
+        this.router.post(`${this.baseUrl}/GetTableData`, async (req: Request, res: Response, next: NextFunction) => {
             await this.getAllEmployees(req, res, next);
         });
 
-        this.router.post(this.baseUrl, async(req, res, next) => {
+        this.router.post(this.baseUrl, this.getValidationChain() , async(req: Request, res: Response, next: NextFunction) => {
+
+            const validationErrors = validationResult(req);
+            if(!validationErrors.isEmpty()) {
+                return res.status(HttpCode.UNPROCESSABLE_ENTITY).json(validationErrors.array().map(e => e.msg));
+            }
+
             await this.add(req, res, next);
         });
 
-        this.router.put(`${this.baseUrl}/:id`, async(req, res, next) => {
+        this.router.put(`${this.baseUrl}/:id`, this.getValidationChain(), async(req: Request, res: Response, next: NextFunction) => {
+
+            const validationErrors = validationResult(req);
+            if(!validationErrors.isEmpty()) {
+                return res.status(HttpCode.UNPROCESSABLE_ENTITY).json(validationErrors.array().map(e => e.msg));
+            }
+
             const employeeExists = await this._repository.employeeExists(req.params.id);
             if(!employeeExists) {
                 res.status(HttpCode.NOT_FOUND).send();
@@ -33,7 +47,7 @@ export class EmployeeController {
             }            
         });
 
-        this.router.delete(`${this.baseUrl}/:id`, async(req, res, next) => {
+        this.router.delete(`${this.baseUrl}/:id`, async(req: Request, res: Response, next: NextFunction) => {
             const employeeExists = await this._repository.employeeExists(req.params.id);
             if(!employeeExists) {
                 res.status(HttpCode.NOT_FOUND).send();
@@ -83,5 +97,15 @@ export class EmployeeController {
             console.log(error);
             res.status(HttpCode.BAD_REQUEST).send(error.message);
         }
+    }
+
+    private getValidationChain(): ValidationChain[] {
+        return [
+            check("firstName").isLength({ min: 1, max: 255 }).withMessage("First Name cannot be empty"),
+            check("lastName").isLength({ min: 1, max: 255 }).withMessage("Last Name cannot be empty"),
+            check("email").isLength({ min: 1, max: 255 }).withMessage("Email cannot be empty").isEmail().withMessage("Please provide correct email"),
+            check("company").notEmpty().withMessage("Company cannot be empty"),
+            check("active").notEmpty().withMessage("Active cannot be empty"),
+        ]
     }
 }
